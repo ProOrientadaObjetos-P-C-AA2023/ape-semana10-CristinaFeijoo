50fecce949167ce67513bed2c2069ed01138e14e
CristinaFeijoo <115591738+CristinaFeijoo@users.noreply.github.com>
2023-06-20 12:53:37 -0500

    Trabajo individual

--- ManejoHerencia/src/paquete4/PrestamoEducativo.java
+++ ManejoHerencia/src/paquete4/PrestamoEducativo.java
@@ -4,11 +4,67 @@
  * and open the template in the editor.
  */
 package paquete4;
+import Paquete5.Persona;
+import paquete2.Prestamo;
 
-/**
- *
- * @author reroes
- */
-public class PrestamoEducativo {
-    
+public class PrestamoEducativo extends Prestamo {
+    public String nivelEstudio;
+    public InstitutoEducativo centroEdcuativo;
+    public double valorCarrera;
+    public double valorMesPrestamoCarrera;
+
+    public PrestamoEducativo() {
+    }
+
+    public PrestamoEducativo(String nivelEstudio,
+                             InstitutoEducativo centroEdcuativo, double valorCarrera) {
+        this.nivelEstudio = nivelEstudio;
+        this.centroEdcuativo = centroEdcuativo;
+        this.valorCarrera = valorCarrera;
+    }
+
+    public PrestamoEducativo(Persona beneficiario, double timePrestamoMes, String ciudadPrestamo, String nivelEstudio,
+                             InstitutoEducativo centroEdcuativo, double valorCarrera) {
+        super(beneficiario, timePrestamoMes, ciudadPrestamo);
+        this.nivelEstudio = nivelEstudio;
+        this.centroEdcuativo = centroEdcuativo;
+        this.valorCarrera = valorCarrera;
+    }
+
+    public String getNivelEstudio() {
+        return nivelEstudio;
+    }
+
+    public void setNivelEstudio(String nivelEstudio) {
+        this.nivelEstudio = nivelEstudio;
+    }
+
+    public InstitutoEducativo getCentroEdcuativo() {
+        return centroEdcuativo;
+    }
+
+    public void setCentroEdcuativo(InstitutoEducativo centroEdcuativo) {
+        this.centroEdcuativo = centroEdcuativo;
+    }
+
+    public double getValorCarrera() {
+        return valorCarrera;
+    }
+
+    public void calcularValorMesPrestamoCarrera(double valorCarrera) {
+        this.valorCarrera = valorCarrera;
+        this.valorMesPrestamoCarrera=(valorCarrera/timePrestamoMes)-(0.1*(valorCarrera/timePrestamoMes));
+    }
+
+    public double getValorMesPrestamoCarrera() {
+        return valorMesPrestamoCarrera;
+    }
+
+    @Override
+    public String toString() {
+        return "NIVEL DE ESTUDIO'" + nivelEstudio + '\'' +
+                ", CENTRO EDUCATIVO" + centroEdcuativo +
+                ", VALOR CARRERA" + valorCarrera +
+                ", VALOR POR MES DEL PRESTAMO EDUCATIVO" + valorMesPrestamoCarrera;
+    }
 }
