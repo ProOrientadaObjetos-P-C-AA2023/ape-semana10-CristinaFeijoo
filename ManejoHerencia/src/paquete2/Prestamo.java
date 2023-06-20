6ea118c241a3b9b29039b34f2f176165fa6046f7
CristinaFeijoo <115591738+CristinaFeijoo@users.noreply.github.com>
2023-06-20 12:52:34 -0500

    Trabajo individual

--- ManejoHerencia/src/paquete2/Prestamo.java
+++ ManejoHerencia/src/paquete2/Prestamo.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
 
-/**
- *
- * @author reroes
- */
+import Paquete5.Persona;
+
 public class Prestamo {
-    
+    public Persona beneficiario;
+    public double timePrestamoMes;
+    public String ciudadPrestamo;
+
+    public Prestamo() {
+    }
+
+    public Prestamo(Persona beneficiario, double timePrestamoMes, String ciudadPrestamo) {
+        this.beneficiario = beneficiario;
+        this.timePrestamoMes = timePrestamoMes;
+        this.ciudadPrestamo = ciudadPrestamo;
+    }
+
+    public Persona getBeneficiario() {
+        return beneficiario;
+    }
+
+    public double getTimePrestamoMes() {
+        return timePrestamoMes;
+    }
+
+    public String getCiudadPrestamo() {
+        return ciudadPrestamo;
+    }
+
+    public void setBeneficiario(Persona beneficiario) {
+        this.beneficiario = beneficiario;
+    }
+
+    public void setTimePrestamoMes(double timePrestamoMes) {
+        this.timePrestamoMes = timePrestamoMes;
+    }
+
+    public void setCiudadPrestamo(String ciudadPrestamo) {
+        this.ciudadPrestamo = ciudadPrestamo;
+    }
+
+    @Override
+    public String toString() {
+        return "BENEFICIARIO" + beneficiario.toString() +
+                "\n TIEMPO PRESTAMO" + timePrestamoMes +
+                "\n CIUDAD DONDE FUE EL PRESTAMO'" + ciudadPrestamo;
+
+    }
 }
