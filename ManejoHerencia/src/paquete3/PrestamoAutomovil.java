c7d0aacfd7df7ae583b41affe26dd070dfeff0bf
CristinaFeijoo <115591738+CristinaFeijoo@users.noreply.github.com>
2023-06-20 12:53:01 -0500

    Trabajo individual

--- ManejoHerencia/src/paquete3/PrestamoAutomovil.java
+++ ManejoHerencia/src/paquete3/PrestamoAutomovil.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paquete3;
 
-/**
- *
- * @author reroes
- */
-public class PrestamoAutomovil {
-    
+import Paquete5.Persona;
+import paquete2.Prestamo;
+
+public class PrestamoAutomovil extends Prestamo {
+    public String tipoAuto;
+    public String marcaAuto;
+    public Persona garante1;
+    public double valorAuto;
+    public double valorMesPagoPrestamo;
+
+    public PrestamoAutomovil(Persona beneficiario, double timePrestamoMes, String ciudadPrestamo, String tipoAuto, String marcaAuto, Persona garante1, double valorAuto) {
+    }
+
+    public PrestamoAutomovil(String tipoAuto, String marcaAuto, Persona garantel, double valorAuto) {
+        this.tipoAuto = tipoAuto;
+        this.marcaAuto = marcaAuto;
+        this.garante1 = garante1;
+        this.valorAuto = valorAuto;
+    }
+
+    public PrestamoAutomovil(Persona beneficiario, double timePrestamoMes, String ciudadPrestamo, String tipoAuto,
+                             String marcaAuto, Persona garantel, double valorAuto, double valorMesPagoPrestamo) {
+
+        super(beneficiario, timePrestamoMes, ciudadPrestamo);
+        this.tipoAuto = tipoAuto;
+        this.marcaAuto = marcaAuto;
+        this.garante1 = garante1;
+        this.valorAuto = valorAuto;
+        this.valorMesPagoPrestamo = valorMesPagoPrestamo;
+    }
 }
