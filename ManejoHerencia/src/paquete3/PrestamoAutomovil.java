1332b0d3f8779ac009664bb0ad0bb06db209065d
CristinaFeijoo <115591738+CristinaFeijoo@users.noreply.github.com>
2023-06-20 22:48:52 -0500

    Trabajo Individual

--- ManejoHerencia/src/paquete3/PrestamoAutomovil.java
+++ ManejoHerencia/src/paquete3/PrestamoAutomovil.java
@@ -9,26 +9,57 @@ public class PrestamoAutomovil extends Prestamo {
     public String marcaAuto;
     public Persona garante1;
     public double valorAuto;
-    public double valorMesPagoPrestamo;
+    public double valorAuto_PrestamoAuto;
 
-    public PrestamoAutomovil(Persona beneficiario, double timePrestamoMes, String ciudadPrestamo, String tipoAuto, String marcaAuto, Persona garante1, double valorAuto) {
-    }
 
-    public PrestamoAutomovil(String tipoAuto, String marcaAuto, Persona garantel, double valorAuto) {
+    public PrestamoAutomovil(String tipoAuto, String marcaAuto, Persona garante1, double valorAuto) {
         this.tipoAuto = tipoAuto;
         this.marcaAuto = marcaAuto;
         this.garante1 = garante1;
         this.valorAuto = valorAuto;
     }
 
-    public PrestamoAutomovil(Persona beneficiario, double timePrestamoMes, String ciudadPrestamo, String tipoAuto,
-                             String marcaAuto, Persona garantel, double valorAuto, double valorMesPagoPrestamo) {
+    public String getTipoAuto() {
+        return tipoAuto;
+    }
 
-        super(beneficiario, timePrestamoMes, ciudadPrestamo);
+    public void setTipoAuto(String tipoAuto) {
         this.tipoAuto = tipoAuto;
+    }
+
+    public String getMarcaAuto() {
+        return marcaAuto;
+    }
+
+    public void setMarcaAuto(String marcaAuto) {
         this.marcaAuto = marcaAuto;
+    }
+
+    public Persona getGarante1() {
+        return garante1;
+    }
+
+    public void setGarante1(Persona garante1) {
         this.garante1 = garante1;
-        this.valorAuto = valorAuto;
-        this.valorMesPagoPrestamo = valorMesPagoPrestamo;
+    }
+
+    public double getValorAuto() {
+        return valorAuto;
+    }
+
+    public double calcularValorAutoPrestamo() {
+        this.valorAuto_PrestamoAuto=valorAuto/timePrestamoMes;
+        return valorAuto_PrestamoAuto;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+
+         "Prestamo  Automovil" +
+                "\n tipoAuto  " + tipoAuto +
+                "\n marcaAuto  " + marcaAuto +
+                "\n garante1 " + garante1 +
+                "\n valorAuto  " + valorAuto +
+                "\n Valor del auto con el prestamo de meses " + valorAuto_PrestamoAuto;
     }
 }
